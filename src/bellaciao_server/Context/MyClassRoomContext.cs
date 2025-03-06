7bc2b8c8e6db9aa9b987604b88abf8060f2c1823
Vasily <>
2025-03-07 04:03:56 +0700

    Маппер x6

--- src/bellaciao_server/Context/MyClassRoomContext.cs
+++ src/bellaciao_server/Context/MyClassRoomContext.cs
@@ -71,6 +71,7 @@ namespace Context
             {
                 entity.ToTable("lesson_participants");
                 entity.HasKey(e => e.ID);
+                entity.Property(e => e.ID).HasColumnName("id");
                 entity.Property(e => e.UserID).HasColumnName("user_id");
                 entity.Property(e => e.LessonID).HasColumnName("lesson_id");
                 entity.Property(e => e.Role).HasColumnName("role");
@@ -79,7 +80,7 @@ namespace Context
             modelBuilder.Entity<LessonCase>(entity =>
             {
                 entity.ToTable("lesson_cases");
-                entity.HasKey(e => e.ID);
+                entity.Property(e => e.ID).HasColumnName("id");
                 entity.Property(e => e.LessonID).HasColumnName("lesson_id");
                 entity.Property(e => e.UserID).HasColumnName("user_id");
                 entity.Property(e => e.CreatedAt).HasColumnName("created_at");
