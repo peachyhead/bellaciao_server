3ad3bc199afbc908098e3ec8ad294c84f20cf9dd
Vasily <>
2025-03-07 00:11:48 +0700

    Исправил поле x3

--- src/bellaciao_server/Context/MyClassRoomContext.cs
+++ src/bellaciao_server/Context/MyClassRoomContext.cs
@@ -29,7 +29,7 @@ namespace Context
                 entity.HasKey(e => e.id);
                 entity.Property(e => e.id).HasColumnName("id");
                 entity.Property(e => e.title).HasColumnName("title");
-                entity.Property(e => e.headid).HasColumnName("headid");
+                entity.Property(e => e.head_id).HasColumnName("headid");
                 // entity.HasMany(e => e.Participants).WithOne().HasForeignKey("ClassroomId"); // Uncomment if Participants is needed
             });
         }
