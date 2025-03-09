854fffba564748d9120e91ca001ff938ee85fa69
Vasily <>
2025-03-09 13:54:47 +0700

    сертификат

--- src/bellaciao_server/Program.cs
+++ src/bellaciao_server/Program.cs
@@ -3,6 +3,14 @@ using Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5000, listenOptions =>
+    {
+        listenOptions.UseHttps("/home/acid_admin/myclassroom.pfx", "-1usedBan");
+    });
+});
+
 // Configure services
 builder.Services.AddDbContext<MyClassroomContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -29,6 +37,7 @@ if (app.Environment.IsDevelopment())
 // Включаем CORS перед обработкой запросов
 app.UseCors("AllowAll");
 app.UseRouting();
+app.UseHttpsRedirection();
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
